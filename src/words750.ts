6cc9e1ed558625991821783c90e519b080bd99d4
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-16 00:27:23 +0530

    added opening files

--- src/words750.ts
+++ src/words750.ts
@@ -1,13 +1,58 @@
-import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {window, Uri, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import * as fs from 'fs';
 
 export function activate(ctx: ExtensionContext) {
 
     // create a new word counter
     const wordCounter = new WordCounter();
     const controller = new WordCounterController(wordCounter);
-
+    const today = commands.registerCommand('words750.today', () => {
+        const date = getDate();
+        const rootPath = workspace.getConfiguration().get('rootPath')
+        const filePath = `${rootPath}/${date}.words750.txt`;
+        if( ! fs.existsSync(filePath) ){
+            // fs.f
+            // create the file...
+        }
+        workspace.openTextDocument( Uri.file( filePath ) )
+            .then( (doc) => {
+                window.showTextDocument(doc);
+            })        
+    })
+    
+    ctx.subscriptions.push(today);
     ctx.subscriptions.push(controller);
     ctx.subscriptions.push(wordCounter);
+}
+
+/**
+ * Returns the page for a day with the given offset. If the page doesn't exist yet, it will be created (with the current date as header) 
+ * @param {number} offset - 0 is today, -1 is yesterday
+ */
+const getPagePath = (date) => {
+
+    const rootPath = `/Users/admin/.words750`
+    return `${rootPath}/${date}.words750.txt`;
+}
+
+const getDate = () => {
+    var today = new Date();
+    var dd = today.getDate()
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    
+    // if(dd<10) {
+    //     const dds = '0'+ dd.toString()
+    // } 
+    
+    // if(mm<10) {
+    //     const mms = '0'+ mm.toString()
+    // } 
+    
+    console.log('`${yyyy}-${mm}-${dd}`;', `${yyyy}-${mm}-${dd}`)
+    return `${yyyy}-${mm}-${dd}`;    
+    
+
 }
 
 export class WordCounter {
